b76ca343c44534efebeda15504f2535925ae994b
EmirhanSyl <61618968+EmirhanSyl@users.noreply.github.com>
2024-05-18 01:11:48 +0300

    Project Setup Completed

---
+++ clean_panda/data_type_converter.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+class DataTypeConverter:
+    def __init__(self) -> None:
+        '''TODO: Implementation'''
+        pass
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clean_panda',
+    version='0.1.0',
+    description='A package for handling various data preprocessing tasks',
+    author='asimtarik & emirs',
+    author_email='support@cleanpanda.com',
+    url='https://github.com/EmirhanSyl/CinemaConsult',  # Replace with your GitHub repository URL
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        # Add other dependencies
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
